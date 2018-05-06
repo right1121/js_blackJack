289fdf78d8c3fb33c9159890d966727eca5eee4f
MsakiHoshino <right1121@hotmail.co.jp>
2018-05-07 01:18:11 +0900

    Create card class

--- blackJack.js
+++ blackJack.js
@@ -1,13 +1,29 @@
-var generateCard = function(){
-	var cards = [];
-	for (var i = 1; i <= 52; i++) {
-		cards.push(i);
+var playerCards;
+var dealerCards;
+
+class Card {
+	constructor(){
+		this._cards = []
+		for (var i = 1; i <= 52; i++) {
+			this._cards.push(i);
+		}
 	}
-	return cards;
-};
+
+	get remaining(){
+		return this._cards;
+	}
+
+	_shuffleCard(){
+		for(var i = this._cards.length - 1; i > 0; i--){
+	    var r = Math.floor(Math.random() * (i + 1));
+	    var tmp = this._cards[i];
+	    this._cards[i] = this._cards[r];
+	    this._cards[r] = tmp;
+		}
+	}
+}
 
 $(function(){
-var cards = generateCard();
-
-console.log(cards);
+	var cards = new Card();
+	console.log(cards.remaining);
 });
